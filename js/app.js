975fc5706d540c655cdd59d3828cbfaa01737875
Bahodirjon <Bahodirjon@gmail.com>
2025-01-31 06:46:21 +0200

    Bayroq

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
+const loaderContainer = document.querySelector(".loader-container");
+
 async function getData() {
-    const req = await fetch("https://restcountries.com/v3.1/all");
-    const data = await req.json();
-    return data;
+  loaderContainer.classList.remove("hidden");
+  const req = await fetch("https://restcountries.com/v3.1/all");
+  const data = await req.json();
+  loaderContainer.classList.add("hidden");
+
+  return data;
 }
 
 const list = document.querySelector("#list");
 
 function generateCountires(countries) {
-    countries.forEach((c) => {
-        let li = document.createElement("li");
-        li.textContent = c.name.common;
-        list.appendChild(li);
-    });
+  countries.forEach((c) => {
+    console.log(c);
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+    let img = document.querySelector("img");
+    img.src = c.flags.svg;
+    img.width = 300;
+    img.alt = c.flags.alt;
+    p.textContent = c.name.common;
+    li.appendChild(img);
+    li.appendChild(p);
+
+    list.appendChild(li);
+  });
 }
 
 getData()
-    .then((data) => generateCountires(data))
-    .catch((error) => console.log(error));
+  .then((data) => generateCountires(data))
+  .catch((error) => console.log(error));
